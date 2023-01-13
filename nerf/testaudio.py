860c42cf1de564dd0ae5006bc8021dd732eda3f2
Khalil Wong <1039906090@qq.com>
2023-01-13 10:08:29 +0800

    test linux pyaudio

--- nerf/testaudio.py
+++ nerf/testaudio.py
@@ -3,27 +3,38 @@ import numpy as np
 from queue import Queue
 from threading import Thread, Event
 
-import keyboard
+from pynput import keyboard
 
 def _read_frame(stream, exit_event, queue, chunk):
 
     while True:
         if exit_event.is_set():
-            print(f'[INFO] read frame thread ends')
+            print('[INFO] read frame thread ends')
             break
         frame = stream.read(chunk, exception_on_overflow=False)
         frame = np.frombuffer(frame, dtype=np.int16).astype(np.float32) / 32767 # [chunk]
         queue.put(frame)
+        #print(queue.qsize())
 
 def _play_frame(stream, exit_event, queue, chunk):
 
     while True:
         if exit_event.is_set():
-            print(f'[INFO] play frame thread ends')
+            print('[INFO] play frame thread ends')
             break
+        print(queue.qsize())
         frame = queue.get()
         frame = (frame * 32767).astype(np.int16).tobytes()
         stream.write(frame, chunk)
+
+def on_press(key):
+    if key == keyboard.Key.enter:
+        print('you press Enter')
+        return False
+
+def on_release(key):
+    if key == keyboard.Key.enter:
+        print('you release Enter')
 
 exit_event = Event()
 audio_instance = pyaudio.PyAudio()
@@ -38,7 +49,10 @@ process_play_frame = Thread(target=_play_frame, args=(output_stream, exit_event,
 process_read_frame.start()
 process_play_frame.start()
 
-keyboard.wait('q')
+with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
+    listener.join()
+
+#keyboard.wait('q')
 
 exit_event.set()
 output_stream.stop_stream()
